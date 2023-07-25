8cbb4772dff0fa371f8b1c9ad7ce15f84d883d83
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-25 12:38:28 +0200

    a second example

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const http = require("http");
-const host = 'localhost';
+// const http = require("http");
+// const host = 'localhost';
+// const port = 8000;
+
+// const requestListener = function (req, res) {
+//     console.log(req.url);
+//     res.end("hello from Node")
+// };
+
+// const server = http.createServer(requestListener);
+
+// server.listen(port, host, () => {
+//     console.log(`Server is running on http://${host}:${port}`);
+// });
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Extract the name from the URL
+  const name = req.url.substr(1);
+
+  // Set the content type to plain text
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Check if a name is provided in the URL
+  if (name) {
+    res.end(`Hello from ${name}!`);
+  } else {
+    res.end('Hello!');
+  }
+});
+
 const port = 8000;
 
-const requestListener = function (req, res) {
-    console.log(req.url);
-    res.end("hello from Node")
-};
-
-const server = http.createServer(requestListener);
-
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
